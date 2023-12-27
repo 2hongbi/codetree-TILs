7461b05ec26ac7fb99f4c0b0a105ca9c4bddb13b
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-12-27 15:58:02 +0900

    Title: 체크판위에서 2, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231227/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,17 @@
+r, c = map(int, input().split())
+grid = [
+    list(map(str, input()))
+    for _ in range(r)
+]
+
+cnt = 0
+for i in range(1, r):
+    for j in range(1, c):
+        for k in range(i + 1, r - 1):
+            for l in range(j + 1, c - 1):
+                if grid[0][0] != grid[i][j] and \
+                   grid[i][j] != grid[k][l] and \
+                   grid[k][l] != grid[r - 1][c - 1]:
+                    cnt += 1
+                        
+print(cnt)
