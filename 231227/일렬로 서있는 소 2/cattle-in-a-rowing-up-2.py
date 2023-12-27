b2839e283eb70146dbcd2331bbad47782425d162
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-12-27 13:41:44 +0900

    Title: 일렬로 서있는 소 2, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231227/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+cows = list(map(int, input().split()))
+
+ans = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        if cows[i] <= cows[j]:
+            for k in range(j + 1, n):
+                if cows[j] <= cows[k]:
+                    ans += 1    
+
+print(ans)
