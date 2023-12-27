d7dfe3fc5be566e891f1c5855e22caba378d320e
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-12-27 23:48:16 +0900

    Title: 체크판위에서 2, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 231227/체크판위에서 2/on-the-checkboard-2.py
+++ 231227/체크판위에서 2/on-the-checkboard-2.py
@@ -1,17 +1,21 @@
-r, c = map(int, input().split())
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
 grid = [
-    list(map(str, input()))
-    for _ in range(r)
+    input().split()
+    for _ in range(n)
 ]
 
+# 이동 시에 행과 열이 전부 증가하도록
+# 모든 쌍을 다 잡아봅니다.
 cnt = 0
-for i in range(1, r):
-    for j in range(1, c):
-        for k in range(i + 1, r - 1):
-            for l in range(j + 1, c - 1):
+for i in range(1, n):
+    for j in range(1, m):
+        for k in range(i + 1, n - 1):
+            for l in range(j + 1, m - 1):
+                # 그 중 색깔이 전부 달라지는 경우에만 개수를 세줍니다.
                 if grid[0][0] != grid[i][j] and \
                    grid[i][j] != grid[k][l] and \
-                   grid[k][l] != grid[r - 1][c - 1]:
-                        cnt += 1
+                   grid[k][l] != grid[n - 1][m - 1]:
+                    cnt += 1
                         
 print(cnt)
