7aa71d2081f049e157901f6ae4499f40387d7a5c
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-01-04 23:06:19 +0900

    Title: 세 수의 합, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240104/세 수의 합/sum-of-three-num.py
@@ -0,0 +1,22 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+count = dict()
+
+for a in arr:
+    if a in count:
+        count[a] += 1
+    else:
+        count[a] = 1
+
+ans = 0
+for i in range(n):
+    count[arr[i]] -= 1
+
+    for j in range(i):
+        diff = k - arr[i] - arr[j]
+
+        if diff in count:
+            ans += count[diff]
+
+print(ans)
