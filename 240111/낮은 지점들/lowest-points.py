280ae0f5aa74c3250e41d70268e26dfce8a4079b
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-01-11 22:57:49 +0900

    Title: 낮은 지점들, Time: 284ms, Memory: 31MB, Status: Passed - CodeTree

--- 240111/낮은 지점들/lowest-points.py
+++ 240111/낮은 지점들/lowest-points.py
@@ -1,17 +1,16 @@
-from collections import defaultdict
-
 n = int(input())
 
-def default_value():
-    return 100001
-
-coordinate = defaultdict(default_value)
+min_y = {}
 for _ in range(n):
     x, y = map(int, input().split())
-    coordinate[x] = min(y, coordinate[x])
+    
+    if x not in min_y:
+        min_y[x] = y
+    else:
+        min_y[x] = min(min_y[x], y)
 
 total = 0
-for v in coordinate.values():
+for v in min_y.values():
     total += v
 
 print(total)
