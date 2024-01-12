6ebc4be9702447dbf16e412c029f3b425687d1e4
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-01-12 15:07:37 +0900

    Title: 작은 구슬의 이동, Time: 5052ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

---
+++ 240112/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,23 @@
+n, t = map(int, input().split())
+x, y, dir = input().split()
+
+directions = {'U': 0, 'R': 1, 'D': 3, 'L': 2}
+
+dxs, dys = [-1, 0, 0, 1], [0, 1, -1, 0]
+
+def in_range(x, y):
+    return 1 <= x <= n and 1 <= y <= n
+
+dir_num = directions[dir]
+x, y = int(x), int(y)
+
+sec = 0
+while sec != t:
+    nx, ny = x + dxs[dir_num], y + dys[dir_num]
+    if not in_range(nx, ny):
+        dir_num = 3 - dir_num
+        sec += 1
+    x, y = x + dxs[dir_num], y + dys[dir_num]
+    sec += 1
+
+print(x, y)
