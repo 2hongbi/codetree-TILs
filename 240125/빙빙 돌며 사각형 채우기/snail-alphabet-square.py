8dda65b6c5cf2b5627defcff79a4131922440fa5
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-01-25 16:53:19 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240125/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
+
+def fill_grid(n, m):
+    grid = [[0] * m for _ in range(n)]
+
+    x, y = 0, 0
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+    dir_num = 0
+
+    for i in range(n * m):
+        grid[x][y] = chr(65 + (i % 65))
+        nx, ny = x + dxs[dir_num], y + dys[dir_num]
+
+        if not in_range(nx, ny) or grid[nx][ny]!= 0:
+            dir_num = (dir_num + 1) % 4
+            nx, ny = x + dxs[dir_num], y + dys[dir_num]
+        
+        x, y = nx, ny
+    
+    return grid
+
+result = fill_grid(n, m)
+for r in result:
+    for c in r:
+        print(c, end=' ')
+    print()
