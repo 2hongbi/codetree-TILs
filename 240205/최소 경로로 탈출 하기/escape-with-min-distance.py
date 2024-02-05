9d7d37341c56e944624aae45fade464b20749a47
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-02-05 20:39:47 +0900

    Title: 최소 경로로 탈출 하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240205/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 240205/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -5,11 +5,11 @@ grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
-visited = [[False for _ in range(n)] for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(n)]
 
 
 def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
+    return 0 <= x < n and 0 <= y < m
 
 
 def can_go(x, y):
