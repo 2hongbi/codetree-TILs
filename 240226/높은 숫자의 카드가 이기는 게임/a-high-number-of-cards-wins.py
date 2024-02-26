7a146cf326047ce7401e1986e5845047a989f585
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-02-26 17:46:19 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240226/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240226/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -1,18 +1,13 @@
 n = int(input())
 b_cards = [int(input()) for _ in range(n)]
 
-a_cards = []
-for i in range(1, 2 * n + 1):
-    if i not in b_cards:
-        a_cards.append(i)
-
-a_cards.sort(reverse=True)
+a_cards = [i for i in range(1, 2 * n + 1) if i not in b_cards] 
 
 a_score = 0
 
 for i in range(n):
-    a_card = a_cards.pop()
-    b_card = b_cards[i]
+    a_card = a_cards.pop() # 가장 작은 숫자부터 추출
+    b_card = b_cards.pop()
 
     if a_card > b_card:
         a_score += 1
